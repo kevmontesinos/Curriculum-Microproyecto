ab9e17f7b167b37ab8e653ee0858f95e94114089
kevmontesinos <kevin.montesinos@correo.unimet.edu.ve>
2022-02-12 11:28:26 -0400

    formulario de contacto y contacto personal

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-alert("ALERTA");
-console.log("ASDA")
+const contactForm = document.getElementById("contacto-form");
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const mensaje = document.getElementById("mensaje");
+
+contactForm.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const nombreValue = nombre.value;
+    const emailValue = email.value;
+    const mensajeValue = mensaje.value;
+
+    if (!nombreValue | !emailValue | !mensajeValue) {
+        alert("Debe completar todos los campos.");
+        return;
+    }
+    
+    console.log(nombreValue);
+    console.log(emailValue);
+    console.log(mensajeValue);
+
+    alert("Su solicitud ha sido enviada.")
+});
